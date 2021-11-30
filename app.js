4f9997e04bc5ee78039a23a9f7ed2f76c12ca84d
squagalings <justineyongjzxy@gmail.com>
2021-11-29 16:43:44 -0800

    lesson 2 part 1

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const app = Vue.createApp()
+const app = Vue.createApp({
+    data(){
+        return {
+            title: 'how i draw faces',
+            author: 'doodleaf',
+            age: 13
+        }
+    },
+    methods: {
+        changeTitle(title) {
+            this.title = title
+        }
+    }
+
+
+})
 
 app.mount('#app')
