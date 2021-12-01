44de1eee9eb7bd33fae294b099085b82cc4e43ac
squagalings <justineyongjzxy@gmail.com>
2021-12-01 15:45:28 -0800

    lesson-3 part 2, continued from last one

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const app = Vue.createApp({
     data(){
         return {
-            title: 'how i draw faces',
-            author: 'doodleaf',
-            age: 13
+            showBooks: true,
+            books: [
+                {title: 'how i draw faces', author: 'doodleaf', img: 'assets/1.jpg', isFav: true},
+                {title: 'guide to animaticion', author: 'doodleaf',img: 'assets/2.jpg', isFav: false},
+                {title: 'wevideo tips & tricks', author: 'doodleaf', img: 'assets/3.jpg', isFav: true},
+            ]
+            
         }
     },
     methods: {
-        changeTitle(title) {
-            this.title = title
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        toggleFav(book){
+            book.isFav = !book.isFav
         }
-    }
+       
+        },
+        computed: {
+            filteredBooks(){
+                return this.books.filter((book) => book.isFav)
+            }
+        }
 
 
 })
