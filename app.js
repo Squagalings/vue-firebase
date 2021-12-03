6157e5de37dbe0f0286d9c4aa2f48fc6a93d3d60
squagalings <justineyongjzxy@gmail.com>
2021-12-02 17:57:42 -0800

    lesson 4 stuff

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = Vue.createApp({
         return {
             showBooks: true,
             books: [
-                {title: 'how i draw faces', author: 'doodleaf', img: 'assets/1.jpg', isFav: true},
-                {title: 'guide to animaticion', author: 'doodleaf',img: 'assets/2.jpg', isFav: false},
-                {title: 'wevideo tips & tricks', author: 'doodleaf', img: 'assets/3.jpg', isFav: true},
+                {title: 'outfit ideas', author: 'doodleaf', img: 'assets/1.jpg', isFav: true},
+                {title: 'sketching expressions', author: 'doodleaf',img: 'assets/2.jpg', isFav: false},
+                {title: 'drawing poses', author: 'doodleaf', img: 'assets/3.jpg', isFav: true},
             ]
             
         }
